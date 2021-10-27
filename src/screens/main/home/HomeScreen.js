923107869eb168272c24aa28072896bc3dab5142
anhlhde140207 <lamhoanganh2000@gmail.com>
2021-10-27 20:39:27 +0700

    add image

--- src/screens/main/home/HomeScreen.js
+++ src/screens/main/home/HomeScreen.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Button, Text, View } from "react-native";
 import { SafeAreaView } from "react-native";
+import { useSelector } from "react-redux";
 
 const HomeScreen = ({ navigation }) => {
+  const usersData = useSelector((state) => state.users);
+  const postsData = useSelector((state) => state.posts);
+  console.log(usersData, postsData);
   return (
     <SafeAreaView>
       <Text>this is Home screen</Text>
