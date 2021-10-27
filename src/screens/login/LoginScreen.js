0b76a58cab76cec7aced1817d583e9ad1aaefc2b
anhlhde140207 <lamhoanganh2000@gmail.com>
2021-10-27 19:53:21 +0700

    update login with facebook

--- src/screens/login/LoginScreen.js
+++ src/screens/login/LoginScreen.js
@@ -11,35 +11,35 @@ const LoginScreen = () => {
     dispatch(action);
   };
 
-  async function logIn() {
-    try {
-      await Facebook.initializeAsync({
-        appId: "171413448445007",
-      });
-      const { type, token, expirationDate, permissions, declinedPermissions } =
-        await Facebook.logInWithReadPermissionsAsync({
-          permissions: ["public_profile"],
-        });
-      if (type === "success") {
-        // Get the user's name using Facebook's Graph API
-        const response = await fetch(
-          `https://graph.facebook.com/me?fields=id,name,email,picture&access_token=${token}`
-        );
-        login();
-        console.log(await response.json());
-      } else {
-        // type === 'cancel'
-        Alert.alert("Login Fail");
-      }
-    } catch ({ message }) {
-      Alert.alert(`Facebook Login Error: ${message}`);
-    }
-  }
+  // async function logIn() {
+  //   try {
+  //     await Facebook.initializeAsync({
+  //       appId: "171413448445007",
+  //     });
+  //     const { type, token, expirationDate, permissions, declinedPermissions } =
+  //       await Facebook.logInWithReadPermissionsAsync({
+  //         permissions: ["public_profile"],
+  //       });
+  //     if (type === "success") {
+  //       // Get the user's name using Facebook's Graph API
+  //       const response = await fetch(
+  //         `https://graph.facebook.com/me?fields=id,name,email,picture&access_token=${token}`
+  //       );
+  //       login();
+  //       console.log(await response.json());
+  //     } else {
+  //       // type === 'cancel'
+  //       Alert.alert("Login Fail");
+  //     }
+  //   } catch ({ message }) {
+  //     Alert.alert(`Facebook Login Error: ${message}`);
+  //   }
+  // }
 
   return (
     <SafeAreaView>
       <Text>this is login screen</Text>
-      <Button title="Login" onPress={() => logIn()} />
+      <Button title="Login" onPress={() => login()} />
     </SafeAreaView>
   );
 };
